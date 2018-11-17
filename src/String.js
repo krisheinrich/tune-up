1e2a23df7d751ea4f4997ba4574c5fb2ae64410d
krisheinrich <krisheinrich@gmail.com>
2018-11-16 18:45:55 -0800

    Add audio element and handlers for playing and resetting

--- src/String.js
+++ src/String.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import './String.css';
 
-const String = ({ name, onClick }) => (
-  <button className="string" onClick={onClick}>{ name }</button>
-);
+class String extends Component {
+  constructor(props) {
+    super(props);
+    this.resetAudio = this.resetAudio.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  resetAudio() {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+  }
+  handleClick() {
+    this.audio.play();
+    setTimeout(this.resetAudio, 5000);
+  }
+  render() {
+    const { name, src } = this.props;
+    return <div className="string">
+      <button onClick={this.handleClick}>{ name }</button>
+      <audio ref={node => this.audio = node} src={src}
+        vol="0.10" autoPlay={false} preload="true" />
+    </div>;
+  }
+}
 
 export default String;
