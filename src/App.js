9bacb683eff6518c20d504378f65f44f20577979
krisheinrich <krisheinrich@gmail.com>
2018-11-16 17:50:47 -0800

    Add String buttons to the app; remove default header styles and logo/link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import String from './String';
 import './App.css';
+
+const NOTES = 'EADGBE'.split('');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Tune Up</h1>
         </header>
+        <section className="string-buttons">
+          { NOTES.map((name, i) =>
+            <String name={name} onClick={()=>console.log("clicked "+name)} />
+          ) }
+        </section>
       </div>
     );
   }
